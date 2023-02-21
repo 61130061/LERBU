97eabd4b9e5236a7c3a9310818f367a5faabe30e
61130061 <thezeroyou@gmail.com>
2023-02-21 12:07:57 +0700

    ➕ add badge w/ icon component

--- src/components/Badge/Basic.jsx
+++ src/components/Badge/Basic.jsx
@@ -7,36 +7,36 @@ function Basic () {
   return (
     <div className="flex flex-col gap-5 items-center">
       <div className="flex gap-2">
-        <span className="items-center bg-zinc-700 rounded-full px-3 text-sm text-white py-1 font-medium">
+        <span className="bg-zinc-700 rounded-full px-3 text-sm text-white py-1 font-medium">
           Default
         </span>
-        <span className="items-center bg-blue-600 rounded-full px-3 text-sm text-white py-1 font-medium">
+        <span className="bg-blue-600 rounded-full px-3 text-sm text-white py-1 font-medium">
           Primary
         </span>
-        <span className="items-center bg-green-600 rounded-full px-3 text-sm text-white py-1 font-medium">
+        <span className="bg-green-600 rounded-full px-3 text-sm text-white py-1 font-medium">
           Success
         </span>
-        <span className="items-center bg-red-600 rounded-full px-3 text-sm text-white py-1 font-medium">
+        <span className="bg-red-600 rounded-full px-3 text-sm text-white py-1 font-medium">
           Danger
         </span>
-        <span className="items-center bg-yellow-600 rounded-full px-3 text-sm text-white py-1 font-medium">
+        <span className="bg-yellow-600 rounded-full px-3 text-sm text-white py-1 font-medium">
           Warning
         </span>
       </div>
       <div className="flex gap-2">
-        <span className="items-center border border-zinc-700 bg-opacity-20 bg-zinc-600 rounded-full px-3 text-sm text-zinc-900 dark:text-white py-1 font-medium">
+        <span className="border border-zinc-700 bg-opacity-20 bg-zinc-600 rounded-full px-3 text-sm text-zinc-900 dark:text-white py-1 font-medium">
           Default
         </span>
-        <span className="items-center border border-blue-600 bg-opacity-20 bg-blue-600 rounded-full px-3 text-sm text-blue-600 py-1 font-medium">
+        <span className="border border-blue-600 bg-opacity-20 bg-blue-600 rounded-full px-3 text-sm text-blue-600 py-1 font-medium">
           Primary
         </span>
-        <span className="items-center border border-green-600 bg-opacity-20 bg-green-600 rounded-full px-3 text-sm text-green-600 py-1 font-medium">
+        <span className="border border-green-600 bg-opacity-20 bg-green-600 rounded-full px-3 text-sm text-green-600 py-1 font-medium">
           Success
         </span>
-        <span className="items-center border border-red-600 bg-opacity-20 bg-red-600 rounded-full px-3 text-sm text-red-600 py-1 font-medium">
+        <span className="border border-red-600 bg-opacity-20 bg-red-600 rounded-full px-3 text-sm text-red-600 py-1 font-medium">
           Danger
         </span>
-        <span className="items-center border border-yellow-600 bg-opacity-20 bg-yellow-600 rounded-full px-3 text-sm text-yellow-600 py-1 font-medium">
+        <span className="border border-yellow-600 bg-opacity-20 bg-yellow-600 rounded-full px-3 text-sm text-yellow-600 py-1 font-medium">
           Warning
         </span>
       </div>
@@ -57,36 +57,36 @@ function Playground () {
       {code &&
         <CodeBlock language="html">
 {`<!-- Basic badge -->
-<span className="items-center bg-zinc-700 rounded-full px-3 text-sm text-white py-1 font-medium">
+<span className="bg-zinc-700 rounded-full px-3 text-sm text-white py-1 font-medium">
   Default
 </span>
-<span className="items-center bg-blue-600 rounded-full px-3 text-sm text-white py-1 font-medium">
+<span className="bg-blue-600 rounded-full px-3 text-sm text-white py-1 font-medium">
   Primary
 </span>
-<span className="items-center bg-green-600 rounded-full px-3 text-sm text-white py-1 font-medium">
+<span className="bg-green-600 rounded-full px-3 text-sm text-white py-1 font-medium">
   Success
 </span>
-<span className="items-center bg-red-600 rounded-full px-3 text-sm text-white py-1 font-medium">
+<span className="bg-red-600 rounded-full px-3 text-sm text-white py-1 font-medium">
   Danger
 </span>
-<span className="items-center bg-yellow-600 rounded-full px-3 text-sm text-white py-1 font-medium">
+<span className="bg-yellow-600 rounded-full px-3 text-sm text-white py-1 font-medium">
   Warning
 </span>
 
 <!-- Outlined badge -->
-<span className="items-center border border-zinc-700 bg-opacity-20 bg-zinc-600 rounded-full px-3 text-sm text-zinc-900 dark:text-white py-1 font-medium">
+<span className="border border-zinc-700 bg-opacity-20 bg-zinc-600 rounded-full px-3 text-sm text-zinc-900 dark:text-white py-1 font-medium">
   Default
 </span>
-<span className="items-center border border-blue-600 bg-opacity-20 bg-blue-600 rounded-full px-3 text-sm text-blue-600 py-1 font-medium">
+<span className="border border-blue-600 bg-opacity-20 bg-blue-600 rounded-full px-3 text-sm text-blue-600 py-1 font-medium">
   Primary
 </span>
-<span className="items-center border border-green-600 bg-opacity-20 bg-green-600 rounded-full px-3 text-sm text-green-600 py-1 font-medium">
+<span className="border border-green-600 bg-opacity-20 bg-green-600 rounded-full px-3 text-sm text-green-600 py-1 font-medium">
   Success
 </span>
-<span className="items-center border border-red-600 bg-opacity-20 bg-red-600 rounded-full px-3 text-sm text-red-600 py-1 font-medium">
+<span className="border border-red-600 bg-opacity-20 bg-red-600 rounded-full px-3 text-sm text-red-600 py-1 font-medium">
   Danger
 </span>
-<span className="items-center border border-yellow-600 bg-opacity-20 bg-yellow-600 rounded-full px-3 text-sm text-yellow-600 py-1 font-medium">
+<span className="border border-yellow-600 bg-opacity-20 bg-yellow-600 rounded-full px-3 text-sm text-yellow-600 py-1 font-medium">
   Warning
 </span>`}
         </CodeBlock>
